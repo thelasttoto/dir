9fb9bdb3e80e1ef9b90b42d7398e4f01f15b65a4
Ramiz Polic <rpolic@cisco.com>
2025-07-30 19:15:24 +0800

    fix: default hub domain change (#253)
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- hub/config/constants.go
+++ hub/config/constants.go
@@ -9,7 +9,8 @@ const (
 	LocalWebserverPort = 48043
 
 	// DefaultHubAddress is the default URL for the Agent Hub service.
-	DefaultHubAddress = "https://hub.agntcy.org"
+	DefaultHubAddress = "https://agent-directory.outshift.com"
+
 	// DefaultHubBackendGRPCPort is the default gRPC port for the Agent Hub backend.
 	DefaultHubBackendGRPCPort = 443
 )
