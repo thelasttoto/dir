c9585955adacf3af5f6879e08038508218a6dcc5
Ramiz Polic <rpolic@cisco.com>
2025-09-05 23:00:51 +0800

    chore: update brew formula version (#372)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- HomebrewFormula/dirctl.rb
+++ HomebrewFormula/dirctl.rb
@@ -1,7 +1,7 @@
 class Dirctl < Formula
     desc "Command-line interface for AGNTCY directory"
     homepage "https://github.com/agntcy/dir"
-    version "v0.2.9"
+    version "v0.2.11"
     license "Apache-2.0"
     version_scheme 1
 
@@ -12,7 +12,7 @@ class Dirctl < Formula
     on_macos do
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-darwin-arm64"
-            sha256 "ddf2c256231fa1c298dc2a43e67c66890b85eb662c2752b8d1b7abe3282566dc"
+            sha256 "5bf4f665dbef82ab3c5df24b67fb2079dc43093f164aa6852724c768d23951a4"
 
             def install
                 bin.install "dirctl-darwin-arm64" => "dirctl"
@@ -24,7 +24,7 @@ class Dirctl < Formula
 
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-darwin-amd64"
-            sha256 "2eaa1120468d616d4d9f68e10835ec784902fafcb69507e958a6a3c71554ebc6"
+            sha256 "653f2542ea1bdf78c4534a9c15792333e0eb05d536314acfc59881e36197f650"
 
             def install
                 bin.install "dirctl-darwin-amd64" => "dirctl"
@@ -38,7 +38,7 @@ class Dirctl < Formula
     on_linux do
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-linux-arm64"
-            sha256 "1902194a4306a451958e67e6291a62263cfb60a3d904d94bf6404fb5cceb6e4c"
+            sha256 "755766c5df8153649453c2c016c7e104174168aaf39d4fdf27bf124ed0009024"
 
             def install
                 bin.install "dirctl-linux-arm64" => "dirctl"
@@ -50,7 +50,7 @@ class Dirctl < Formula
 
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-linux-amd64"
-            sha256 "6641929ae7615beab67efb3073f43effed011091ff39aabba45a439d7ecbac2e"
+            sha256 "ef55cf632af624c03b1f53cce0a8e9f2a902253fde7f3807e4bdaf9f347a985c"
 
             def install
                 bin.install "dirctl-linux-amd64" => "dirctl"
