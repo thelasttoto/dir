e7ca0b1a661a23e1656be28117d21391b5ce4dc4
Ramiz Polic <rpolic@cisco.com>
2025-09-19 20:57:47 +0800

    chore: bump brew tap to v0.3.0 (#435)
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- HomebrewFormula/dirctl.rb
+++ HomebrewFormula/dirctl.rb
@@ -1,7 +1,7 @@
 class Dirctl < Formula
     desc "Command-line interface for AGNTCY directory"
     homepage "https://github.com/agntcy/dir"
-    version "v0.2.11"
+    version "v0.3.0"
     license "Apache-2.0"
     version_scheme 1
 
@@ -12,7 +12,7 @@ class Dirctl < Formula
     on_macos do
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-darwin-arm64"
-            sha256 "5bf4f665dbef82ab3c5df24b67fb2079dc43093f164aa6852724c768d23951a4"
+            sha256 "6668ba123e67cb3b13da7aa782268bfd5cfec2ea757c006c8e959ff7e78eb611"
 
             def install
                 bin.install "dirctl-darwin-arm64" => "dirctl"
@@ -24,7 +24,7 @@ class Dirctl < Formula
 
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-darwin-amd64"
-            sha256 "653f2542ea1bdf78c4534a9c15792333e0eb05d536314acfc59881e36197f650"
+            sha256 "df1af977204b332e3b8e8fc4e308677b360b3b64c0a7c7fe64824f25b47fa74e"
 
             def install
                 bin.install "dirctl-darwin-amd64" => "dirctl"
@@ -38,7 +38,7 @@ class Dirctl < Formula
     on_linux do
         if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-linux-arm64"
-            sha256 "755766c5df8153649453c2c016c7e104174168aaf39d4fdf27bf124ed0009024"
+            sha256 "93fc407b680b6e3e17e18f6e27863f274624cffc9cab79093be8ed6f24e50c83"
 
             def install
                 bin.install "dirctl-linux-arm64" => "dirctl"
@@ -50,7 +50,7 @@ class Dirctl < Formula
 
         if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
             url "#{url}/dirctl-linux-amd64"
-            sha256 "ef55cf632af624c03b1f53cce0a8e9f2a902253fde7f3807e4bdaf9f347a985c"
+            sha256 "c2b8a1c61700e58df1ebb56b877b1fc044b7a0d8d0899c710a3d3b7718b6238c"
 
             def install
                 bin.install "dirctl-linux-amd64" => "dirctl"
